f0979a81c472a9d2324f8221dca96587925a0da7
Makvi-droid <pasbenauro05@gmail.com>
2024-09-03 23:15:11 +0800

    added new files

---
+++ JS-refresh/function.js
@@ -0,0 +1,37 @@
+function favoriteAnimal(animal){
+
+    return animal;
+
+}
+
+console.log("My favorite animal is a " + favoriteAnimal("lion"));
+
+//add function
+function add7(a){
+    return a + 7;
+}
+console.log(add7(10));
+
+//multiply function
+function multiply(num1, num2){
+    return num1 * num2;
+}
+console.log(multiply(5, 6));
+
+
+//return only 1st letter capital
+function capitalize(string){
+    return string.toUpperCase().charAt(0) + string.slice(1).toLowerCase();
+}
+
+console.log(capitalize("jaVaScRiPT"));
+
+//return only the last letter
+function lastLetter(str){
+    return str.slice(-1);
+}
+
+console.log(lastLetter("Kimiya"));
+
+
+
