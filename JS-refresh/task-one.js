ef17a213f2feb6e20d8c7fcd35c7db6e23bb884e
Makvi-droid <pasbenauro05@gmail.com>
2024-09-03 21:56:52 +0800

    added new files for function topic practice

--- JS-refresh/task-one.js
+++ JS-refresh/task-one.js
@@ -8,3 +8,11 @@ console.log(age);
 console.log(firstName + " " + lastName);
 console.log((3 + 2) - 76 * (1 + 1));
 console.log((4 + 6 + 9) / 77);
+
+let a = 10;
+console.log(9 * a);
+
+const max = 57;
+let actual = max - 13;
+let percentage = actual / max;
+console.log(percentage);
