50fdf68eefac0842652ca9c4e2e7e36d13da0251
Makvi-droid <pasbenauro05@gmail.com>
2024-09-03 21:43:01 +0800

    testing variable knowledge

--- JS-refresh/task-one.js
+++ JS-refresh/task-one.js
@@ -1,4 +1,10 @@
+//refresher practice with variables
+
 let firstName = "Kimiya";
 let lastName = "Higashiyama";
+let age = 21;
 
+console.log(age);
 console.log(firstName + " " + lastName);
+console.log((3 + 2) - 76 * (1 + 1));
+console.log((4 + 6 + 9) / 77);
