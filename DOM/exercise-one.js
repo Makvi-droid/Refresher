bd0e8ef5fb9b13b9ca482311627a0a2abdaa38d5
Makvi-droid <pasbenauro05@gmail.com>
2024-09-06 20:22:40 +0800

    exercise done

--- DOM/exercise-one.js
+++ DOM/exercise-one.js
@@ -1,11 +1,3 @@
-const container = document.querySelector("#container");
-
-const content = document.createElement("div");
-content.classList.add("content");
-content.textContent = "This is the glorious text-content!";
-
-container.appendChild(content);
-
 let paragraph = document.querySelector('p');
 paragraph.textContent = "Hey Im red!";
 paragraph.style.color = 'red';
@@ -15,3 +7,17 @@ let headingThree = document.querySelector('h3');
 headingThree.textContent = 'Im a blue h3!';
 headingThree.style.color = 'blue';
 headingThree.style.fontSize = '2.8rem';
+
+const div = document.createElement("div");
+div.style.border = '5px solid black';
+div.style.backgroundColor = 'pink';
+document.body.appendChild(div);
+
+let h1 = document.createElement('h1');
+h1.textContent = 'Im in a div!';
+div.appendChild(h1);
+
+let p = document.createElement('p');
+p.textContent = 'Me too!';
+p.style.fontSize = '1.8rem';
+div.appendChild(p);
