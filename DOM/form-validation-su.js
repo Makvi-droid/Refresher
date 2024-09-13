bc9418acee621d19a57eba74c599259d0456dd91
Makvi-droid <pasbenauro05@gmail.com>
2024-09-14 07:52:54 +0800

    connected html tags to js file for DOM manipulation

---
+++ DOM/form-validation-su.js
@@ -0,0 +1,6 @@
+const username =  document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const reEnterPassword = document.getElementById('re-enter-password');
+const submitBtn = document.getElementById("submitBtn");
+
