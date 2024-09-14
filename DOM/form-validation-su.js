030bfc1323d9f72abeabdbb37f928da51883884e
Makvi-droid <pasbenauro05@gmail.com>
2024-09-14 08:34:38 +0800

    continuing form validation

--- DOM/form-validation-su.js
+++ DOM/form-validation-su.js
@@ -3,4 +3,9 @@ const email = document.getElementById('email');
 const password = document.getElementById('password');
 const reEnterPassword = document.getElementById('re-enter-password');
 const submitBtn = document.getElementById("submitBtn");
+const form = document.getElementById('form');
 
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+});
